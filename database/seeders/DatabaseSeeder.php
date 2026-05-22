239783ab44aa5c0fe2ad68145e73d5b0b92da533
DYLANeay <dylan.eray6@gmail.com>
2026-05-22 11:24:39 +0200

    update database seeder to add collectseeder inside

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CompanySeeder::class,
             PlaceSeeder::class,
+            CollectSeeder::class,
         ]);
     }
 }
