3738edd3a2363c4e8351fd20e94d6e63458584cd
DYLANeay <dylan.eray6@gmail.com>
2026-05-22 13:55:03 +0200

    update model to match relations, and add tropheeSeeder to the global seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             CompanySeeder::class,
             PlaceSeeder::class,
             CollectSeeder::class,
+            TropheeSeeder::class,
         ]);
     }
 }
