46598938b1163c7124768e11411236860cb232c1
kappa-i <gaby.kappai@gmail.com>
2026-05-22 11:41:10 +0200

    add co-branded page

---
+++ resources/js/pages/CoBranded/Inscription.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Head } from '@inertiajs/vue3'
+
+defineProps({
+    brandName: String,
+    token: String,
+})
+</script>
+
+<template>
+    <Head title="Inscription au don" />
+    <div>
+        <h1>Inscription au don</h1>
+    </div>
+</template>
---
+++ resources/js/pages/CoBranded/Jeu.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Head } from '@inertiajs/vue3'
+
+defineProps({
+    brandName: String,
+    token: String,
+})
+</script>
+
+<template>
+    <Head title="Êtes-vous éligible ?" />
+    <div>
+        <h1>Êtes-vous éligible ?</h1>
+    </div>
+</template>
