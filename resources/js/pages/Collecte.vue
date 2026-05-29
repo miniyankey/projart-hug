bc5e0366c1db1a96fc8f0948c41829701f633cfc
DYLANeay <dylan.eray6@gmail.com>
2026-05-29 11:10:32 +0200

    add text for the collect page and form

--- resources/js/pages/Collecte.vue
+++ resources/js/pages/Collecte.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
 import { useI18n } from 'vue-i18n';
+import CollectForm from '@/components/CollectForm.vue';
 import PublicLayout from '@/layouts/PublicLayout.vue';
 
 const { t } = useI18n();
@@ -8,11 +9,18 @@ const { t } = useI18n();
 
 <template>
     <PublicLayout>
-        <Head :title="t('nav.cta_creer_collecte')" />
-        <div class="mx-auto max-w-7xl px-4 py-10 sm:px-6 sm:py-16">
+        <Head :title="t('collecte.title')" />
+        <div class="mx-auto max-w-3xl px-4 py-10 sm:px-6 sm:py-16">
             <h1 class="text-2xl font-semibold text-gray-900 sm:text-3xl">
-                {{ t('nav.cta_creer_collecte') }}
+                {{ t('collecte.title') }}
             </h1>
+            <p class="mt-4 text-base leading-relaxed text-gray-600">
+                {{ t('collecte.intro') }}
+            </p>
+
+            <div class="mt-10">
+                <CollectForm />
+            </div>
         </div>
     </PublicLayout>
 </template>
