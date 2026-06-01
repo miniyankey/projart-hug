508e5f838c4dcbbdfbdd5ea742531791e2319e09
DYLANeay <dylan.eray6@gmail.com>
2026-06-01 09:45:55 +0200

    create edit entreprise and also update component (since the create and edit pages are almost the sames)

--- resources/js/pages/Admin/Entreprises/Edit.vue
+++ resources/js/pages/Admin/Entreprises/Edit.vue
@@ -1,14 +1,42 @@
 <script setup>
-import { Head } from '@inertiajs/vue3';
+import { Link } from '@inertiajs/vue3';
+import { ArrowLeft } from 'lucide-vue-next';
+import { useI18n } from 'vue-i18n';
+import CompanyForm from '@/components/admin/CompanyForm.vue';
+import { Button } from '@/components/ui/button';
+import AdminLayout from '@/layouts/AdminLayout.vue';
+import { index, update } from '@/routes/admin/entreprises';
 
-defineProps({
-    entreprise: Object,
+const { t } = useI18n();
+
+const props = defineProps({
+    company: {
+        type: Object,
+        required: true,
+    },
 });
 </script>
 
 <template>
-    <Head title="Modifier une entreprise - Administration" />
-    <div>
-        <h1>Modifier une entreprise</h1>
-    </div>
+    <AdminLayout :title="t('admin.entreprises.edit_page.title')">
+        <template #subtitle>{{
+            t('admin.entreprises.edit_page.subtitle')
+        }}</template>
+
+        <template #actions>
+            <Button as-child variant="outline">
+                <Link :href="index.url()">
+                    <ArrowLeft class="size-4" />
+                    {{ t('admin.entreprises.create.cancel') }}
+                </Link>
+            </Button>
+        </template>
+
+        <CompanyForm
+            method="put"
+            :company="props.company"
+            :submit-url="update(props.company.id).url"
+            :submit-label="t('admin.entreprises.edit_page.submit')"
+        />
+    </AdminLayout>
 </template>
