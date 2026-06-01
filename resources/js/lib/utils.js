d09f44c9fc49c07fc3aa81a07d941ba9de2418b6
DYLANeay <dylan.eray6@gmail.com>
2026-06-01 14:14:03 +0200

    remove date related to use their own composable

--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -4,19 +4,3 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs) {
     return twMerge(clsx(inputs));
 }
-
-export function toUrl(href) {
-    return typeof href === 'string' ? href : href?.url;
-}
-
-export function formatDay(day) {
-    if (!day) {
-        return '-';
-    }
-
-    return new Date(day).toLocaleDateString('fr-CH', {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric',
-    });
-}
