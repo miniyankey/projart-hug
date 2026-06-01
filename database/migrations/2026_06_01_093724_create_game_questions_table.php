9ff821d5ffc13b6a1fedac0cdbc2912ea69ade4e
miniyankey <yannisrochat@gmail.com>
2026-06-01 14:03:31 +0200

    Renommage des migrations pour mieux les identifier

--- database/migrations/2026_06_01_093724_create_game_questions_table.php
+++ database/migrations/2026_06_01_093724_create_game_questions_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('questions', function (Blueprint $table) {
+        Schema::create('game_questions', function (Blueprint $table) {
             $table->id();
             $table->string('titre');
             $table->text('question');
@@ -21,6 +21,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('questions');
+        Schema::dropIfExists('game_questions');
     }
 };
