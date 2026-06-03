5f321d2d0e7efa4e6a399f169bdfbf920bd7f489
DYLANeay <dylan.eray6@gmail.com>
2026-06-03 14:07:10 +0200

    Schedule eligibility reminders at 09:00 daily

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Rappels d'éligibilité : envoyés le matin du jour de ré-éligibilité
+Schedule::command('app:send-eligibility-reminders')->dailyAt('09:00');
